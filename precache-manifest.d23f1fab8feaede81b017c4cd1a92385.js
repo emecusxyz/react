ffc8bfb571a7fa10c892cb5166ba956467489b04
emecusxyz <112563553+emecusxyz@users.noreply.github.com>
2023-06-01 18:32:31 +0100

    react build upload

---
+++ precache-manifest.d23f1fab8feaede81b017c4cd1a92385.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "01024d0d70c23b45a094",
+    "url": "/static/css/main.cbcd7e96.chunk.css"
+  },
+  {
+    "revision": "01024d0d70c23b45a094",
+    "url": "/static/js/main.01024d0d.chunk.js"
+  },
+  {
+    "revision": "4587b35573b7dda0a77d",
+    "url": "/static/js/1.4587b355.chunk.js"
+  },
+  {
+    "revision": "4ea73d5ac7db8c3f3474",
+    "url": "/static/js/2.4ea73d5a.chunk.js"
+  },
+  {
+    "revision": "1f46550c42e737540ce6",
+    "url": "/static/js/runtime~main.1f46550c.js"
+  },
+  {
+    "revision": "d44137361e41f83b0e5991c097c0f84e",
+    "url": "/index.html"
+  }
+];
